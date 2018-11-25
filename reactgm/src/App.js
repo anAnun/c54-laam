c3c4e21edc42f1af1bb0f66a29d62c448e9da932
Benjamin Lee <prizefighter@gmail.com>
2018-11-25 10:47:22 -0800

    wip

---
+++ reactgm/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styles from "./App.module.css";
+
+const gm = window.gm;
+
+class App extends Component {
+  state = {
+    vin: "pending..."
+  };
+
+  componentDidMount() {
+    const vin = gm.info.getVIN();
+    this.setState({ vin });
+    console.log(vin)
+  }
+
+  handleClose = () => {
+    gm.system.closeApp();
+  };
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div>VIN: {this.state.vin}</div>
+        <button onClick={this.handleClose}>Close</button>
+      </div>
+    );
+  }
+}
+
+export default App;
